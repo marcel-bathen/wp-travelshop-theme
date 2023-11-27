0571dd060940aef9ed469a0a1f5bda20e8c7b808
we craft <moin@wecraft.agency>
2023-11-27 14:53:25 +0100

    added default ratio settings

--- template-parts/pm-search/filter-badges.php
+++ template-parts/pm-search/filter-badges.php
@@ -1,88 +1,96 @@
 <?php
-    echo "hi";
+echo "hi";
 
-    $activeFilters = [];
+$activeFilters = [];
 
-    // transport types
-    if (empty($args['transport_types']) && count($args['transport_types']) > 1) {
-        return;
-    }
-
+// transport types
+if (empty($args['transport_types']) && count($args['transport_types']) > 1) {
     $selected = [];
     if (empty($_GET['pm-tr']) === false) {
         $selected = BuildSearch::extractTransportTypes($_GET['pm-tr']);
     }
 
     foreach ($args['transport_types'] as $item) {
-        if ( in_array($item->name, $selected) ) {
-            $activeFilters[]  = [
+        if (in_array($item->name, $selected)) {
+            $activeFilters[] = [
                 'id' => $item->name,
                 'name' => $item->name
             ];
         }
     }
-
-    // categories
-    foreach(TS_FILTERS as $filter) {
-        $fieldname = $filter['fieldname'];
-        $name = $filter['name'];
-
-        $selected = array();
-        if(empty($_GET['pm-c'][$fieldname]) === false && preg_match_all("/[a-zA-Z0-9\-]+(?=[,|\+]?)/", $_GET['pm-c'][$fieldname], $matches) > 0){
-            $selected = empty($matches[0]) ? array() : $matches[0];
-        }
-
-        $childs = [];
-        if(!empty($args['categories'][$fieldname][1])){
-            foreach ($args['categories'][$fieldname][1] as $item) {
-                $childs[$item->id_parent][] = $item;
-            }
-        }
-
-        if ( isset($args['categories'][$fieldname]) ) {
-
-            foreach ($args['categories'][$fieldname][0] as $item) {
-                $has_childs = !empty($childs[$item->id_item]) && count($childs[$item->id_item]) > 1;
-                // open the second level if neccessary
-                if(empty($selected) === false && $has_childs === true){
-                    foreach ($childs[$item->id_item] as $child_item){
-                        if(in_array($child_item->id_item, $selected) === true){
-                            break;
-                        }
-                    }
-                }
-                if ( in_array($item->id_item, $selected) ) {
-                    $activeFilters[] = [
-                        'id' => $item->id_item,
-                        'name' => $item->name
-                    ]
-
-                ?>
-
-                <?php
-                }
-
-                ?>
-                <?php if ($has_childs === true) { ?>
+}
 
 
-                    <?php foreach ($childs[$item->id_item] as $child_item) {
-                        if ( in_array($child_item->id_item, $selected) ) {
-                            $activeFilters[] = [
-                                'id' => $child_item->id_item,
-                                'name' => $child_item->name
-                            ]
-                        ?>
+// board type
+if (empty($args['board_types'])) {
+    $selected = [];
 
+    if (empty($_GET['pm-bt']) === false) {
+        $selected = BuildSearch::extractBoardTypes($_GET['pm-bt']);
+    }
 
-
-                    <?php }
-                    }?>
-                <?php } ?>
-                <?php
-            }
+    foreach ($args['board_types'] as $item) {
+        if (in_array($item->name, $selected)) {
+            $activeFilters[] = [
+                'id' => $item->name,
+                'name' => $item->name
+            ];
         }
     }
+}
+
+
+// categories
+foreach (TS_FILTERS as $filter) {
+    $fieldname = $filter['fieldname'];
+    $name = $filter['name'];
+
+    $selected = array();
+    if (empty($_GET['pm-c'][$fieldname]) === false && preg_match_all("/[a-zA-Z0-9\-]+(?=[,|\+]?)/", $_GET['pm-c'][$fieldname], $matches) > 0) {
+        $selected = empty($matches[0]) ? array() : $matches[0];
+    }
+
+    $childs = [];
+    if (!empty($args['categories'][$fieldname][1])) {
+        foreach ($args['categories'][$fieldname][1] as $item) {
+            $childs[$item->id_parent][] = $item;
+        }
+    }
+
+    if (isset($args['categories'][$fieldname])) {
+
+        foreach ($args['categories'][$fieldname][0] as $item) {
+            $has_childs = !empty($childs[$item->id_item]) && count($childs[$item->id_item]) > 1;
+            // open the second level if neccessary
+            if (empty($selected) === false && $has_childs === true) {
+                foreach ($childs[$item->id_item] as $child_item) {
+                    if (in_array($child_item->id_item, $selected) === true) {
+                        break;
+                    }
+                }
+            }
+            if (in_array($item->id_item, $selected)) {
+                $activeFilters[] = [
+                    'id' => $item->id_item,
+                    'name' => $item->name
+                ];
+            }
+
+            if ($has_childs === true) {
+                foreach ($childs[$item->id_item] as $child_item) {
+                    if (in_array($child_item->id_item, $selected)) {
+                        $activeFilters[] = [
+                            'id' => $child_item->id_item,
+                            'name' => $child_item->name
+                        ];
+
+                    }
+                }
+            }
+
+        }
+    }
+}
 
 
 ?>
