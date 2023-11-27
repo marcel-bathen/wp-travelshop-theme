8aea576b915aab06a3382f9715d7365361d436e2
we craft <moin@wecraft.agency>
2023-11-27 14:40:22 +0100

    added default ratio settings

--- template-parts/pm-search/filter-badges.php
+++ template-parts/pm-search/filter-badges.php
@@ -1,6 +1,24 @@
 <?php
     echo "hi";
+
+    $activeFilters = [];
+
+    foreach(TS_FILTERS as $filter) {
+        $fieldname = $filter['fieldname'];
+        $name = $filter['name'];
+
+        $selected = array();
+        if(empty($_GET['pm-c'][$fieldname]) === false && preg_match_all("/[a-zA-Z0-9\-]+(?=[,|\+]?)/", $_GET['pm-c'][$fieldname], $matches) > 0){
+            $selected = empty($matches[0]) ? array() : $matches[0];
+        }
+    }
+
+
 ?>
+
+<pre>
+    <?php print_r($args); ?>
+</pre>
 <section class="content-block content-block-list-active-filters">
 
 </section>
